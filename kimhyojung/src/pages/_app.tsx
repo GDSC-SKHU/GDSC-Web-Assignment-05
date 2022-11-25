da031593d76624e4b802f6ee6622964b3a702a54
khj0426 <706shin1728@naver.com>
2022-11-25 18:07:47 +0900

    feat:전체페이지에 폰트 적용

--- kimhyojung/src/pages/_app.tsx
+++ kimhyojung/src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import type { AppProps } from 'next/app';
+import GlobalStyle from '../styles/GlobalStyle';
+import '../../public/fonts/font.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps}></Component>
+    </>
+  );
 }
