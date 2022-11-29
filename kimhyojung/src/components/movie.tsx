5250db1b0db6b5e1839ee0bae0a58f31c1a64302
khj0426 <706shin1728@naver.com>
2022-11-30 00:08:28 +0900

    feat:영화 컴포넌트에 티져보는 기능 추가

--- kimhyojung/src/components/movie.tsx
+++ kimhyojung/src/components/movie.tsx
@@ -1,47 +1,65 @@
-import styled from 'styled-components';
-import Movie from '../constants';
+import styled, { StyledComponent } from 'styled-components';
+import { Movie } from '../constants';
+import { render } from 'react-dom';
+import { ChangeEvent, Fragment, useState } from 'react';
+import { ReactDOM } from 'react';
+import React from 'react';
+import MovieTeaser from './movieTeaser';
 
-const StyledMovie = styled.article`
+const StyledMovie = styled.section`
   width: 15%;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-  flex-wrap: nowrap;
-  &:hover {
-    opacity: 0.7;
-  }
+  flex-wrap: wrap;
+  max-height: 350px;
 `;
 
 const StyledmovieName = styled.p`
   width: 100%;
+  font-size: 1.8%;
 `;
 
 const StyledmovieImage = styled.img`
-  vertical-align: bottom;
-  height: 80%;
   max-height: 300px;
   width: 100%;
   object-fit: cover;
   display: flex;
+  border-radius: 6%;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.7;
+  }
 `;
 
-const StyledmovieContent = styled.div`
-  width: 100%;
-  display: flex;
-  opacity: 0;
+const StyledmovieButton = styled.button`
+  color: black;
+  background-color: #f1f1f1;
+  border: none;
 `;
 
 export default function MovieComponent({
   name,
   imgSrc,
   imgAlt,
-  content,
+  movieURL,
 }: Movie) {
+  const [modal, setModal] = useState<Boolean>(false);
+  const showTeaser = () => {
+    if (modal) {
+      setModal(false);
+    } else {
+      setModal(true);
+    }
+  };
   return (
-    <StyledMovie>
-      <StyledmovieImage src={imgSrc} alt={imgAlt} />
-      <StyledmovieName>{name}</StyledmovieName>
-    </StyledMovie>
+    <>
+      <StyledMovie>
+        <StyledmovieImage src={imgSrc} alt={imgAlt} />
+        <StyledmovieName>{name}</StyledmovieName>
+        <StyledmovieButton onClick={showTeaser}>티저보기</StyledmovieButton>
+      </StyledMovie>
+      {modal === true ? MovieTeaser(movieURL, modal, showTeaser) : null}
+    </>
   );
 }
