744702b77d6d0360901c665f21778ce73696d77b
khj0426 <706shin1728@naver.com>
2022-12-02 16:55:28 +0900

    feat:Nav컴포넌트 랜더링

--- kimhyojung/src/pages/_app.tsx
+++ kimhyojung/src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/GlobalStyle';
 import '../../public/fonts/font.css';
-
+import Nav from '../components/nav';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
+      <Nav />
       <Component {...pageProps}></Component>
     </>
   );
