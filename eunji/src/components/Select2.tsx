30be7cae791f0552ba832ca7de39de2a4e6c0822
eunji <eunjijjang11@naver.com>
2022-12-02 16:48:14 +0900

    styled: bookin 영화관 꾸미기

--- eunji/src/components/Select2.tsx
+++ eunji/src/components/Select2.tsx
@@ -6,21 +6,27 @@ export default function Select2() {
     const [number, setNumber] = useState<number>(1);
     const [price, setPrice] = useState<number>(600);
     const [result, setResult] = useState<number>(3);
-    const [name, setName] = useState<string>('');
-    
+    const [name, setName] = useState<string>('Aladin');
+
     const onClickTest = () => {
         if (onclick) {
             `background-color: black;`
         }
     }
-// 만약 클릭하면 result 값이 반영되게
+    // 만약 클릭하면 result 값이 반영되게
 
     const router = useRouter();
 
     // 가격 바뀌게
     const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        const { value } = e.target;
         setPrice(Number(e.target.value));
         // 타겟은 기본적으로 String
+    };
+
+    const onSelectChange2 = (e: ChangeEvent<HTMLSelectElement>) => {
+        const { value } = e.target;
+        setName(e.target.value);
     };
 
     useEffect(() => {
@@ -28,41 +34,156 @@ export default function Select2() {
         setResult(price * actualNumber);
     }, [price, number]);
 
+    const onReset = () => {
+        setNumber(0);
+        setPrice(0);
+    }
+
     const onClickPurchase = () => {
         if (number <= 0) {
             alert('좌석을 선택해 주세요.');
+            onReset();
+            setPrice(600);
             return;
         }
         else router.push({
             pathname: '/purchase',
-            query: {person: number, price: result},
+            query: { name: name, person: number, price: result },
         });
     };
 
     return (
-        <div>
-            <select value={price} onChange={onSelectChange}>
+        <Container>
+            <StyledSelect2 value={price} onChange={onSelectChange}>
                 <option value={600}>Aladin(600원)</option>
                 <option value={700}>Beauty and the Beast(700원)</option>
                 <option value={800}>범죄도시2(800원)</option>
-            </select>
+            </StyledSelect2>
 
-            <UDBtn onClick={() => setNumber(prev => prev + 1)}>up</UDBtn>
-            <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+            <StyledSelect value={name} onChange={onSelectChange2}>
+                <option value={'Aladin'}>Aladin(600원)</option>
+                <option value={'Beauty and the Beast'}>Beauty and the Beast(700원)</option>
+                <option value={'범죄도시2'}>범죄도시2(800원)</option>
+            </StyledSelect>
 
-            <h2>선택하신 {name} 인원수는 {number}명, 총 금액은 {result}원입니다.</h2>
+            <ScreenDiv />
+
+            <StyledDiv>
+                <UDBtn onClick={() => setNumber(prev => prev + 1)}>up</UDBtn>
+                <Seat />
+                <UDBtn onClick={() => setNumber(prev => prev + 1)}>up</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev + 1)}>up</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev + 1)}>up</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev + 1)}>up</UDBtn>
+            </StyledDiv>
+            <StyledDiv>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <Seat />
+                <Seat />
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+            </StyledDiv>
+            <StyledDiv>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+            </StyledDiv>
+            <StyledDiv>
+                <Seat />
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <UDBtn onClick={() => setNumber(prev => prev - 1)}>down</UDBtn>
+                <Seat />
+            </StyledDiv>
+
+            <div>
+                <Occupied />
+                <p>: 이미 선택된 좌석</p>
+            </div>
+
+            <h2>선택하신 인원수는 {number}명, 총 금액은 {result}원입니다.</h2>
 
             <PurchaseBtn onClick={onClickPurchase}>결제하기</PurchaseBtn>
-
-            <Btn onChange={onClickTest}>메뉴1</Btn>
-            <Btn onChange={onClickTest}>메뉴2</Btn>
-        </div>
+        </Container>
     )
 }
 
+const Occupied = styled.div`
+color: #fff;
+width: 25px;
+height: 25px;
+border: 1px solid #dbdee2;
+border-radius: 10px;
+margin-top: 1rem;
+background-color: #404a5c;
+`;
+
+const Seat = styled.div`
+color: #fff;
+width: 40px;
+height: 40px;
+border: 1px solid #dbdee2;
+border-radius: 10px;
+margin: 0 5px;
+background-color: #404a5c;
+`;
+
+const ScreenDiv = styled.div`
+background-color: #E6E6E6;
+width: 20em;
+height: 10em;
+margin-top: 2rem;
+margin-bottom: 1rem;
+border-radius: 1rem;
+box-shadow: 0px 0px 1rem #000;
+`;
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+`;
+
+const StyledDiv = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 1rem;
+`;
+
+const StyledSelect = styled.select`
+display: none;
+`;
+
+const StyledSelect2 = styled.select`
+margin-top: 2rem;
+all: unset;
+border: 1px solid red;
+border-radius: 20px;
+padding: 5px 10px;
+text-align: center;
+transition: 0.2s;
+cursor: pointer;
+
+&:hover {
+    color: red;
+    transform: translateY(-5px);
+}
+`;
+
 const UDBtn = styled.button`
 background-color: #fff;
+width: 40px;
+height: 40px;
 border: 1px solid #dbdee2;
+border-radius: 10px;
+margin: 0 5px;
 color: #404a5c;
 cursor: pointer;
 &:hover {
@@ -71,15 +192,7 @@ cursor: pointer;
 }
 `;
 
-const Btn = styled.button `
-background-color: #fff;
-border: 1px solid #dbdee2;
-color: #404a5c;
-cursor: pointer;
-&:visited {background-color: black;}
-`;
-
-const PurchaseBtn = styled.button `
+const PurchaseBtn = styled.button`
 background-color: black;
 color: white;
 font-size: 1rem;
--- eunji/src/pages/booking.tsx
+++ eunji/src/pages/booking.tsx
@@ -6,13 +6,15 @@ import Select2 from "../components/Select2";
 export default function Booking() {
     return(
         <div>
-            <h1>예매사이트</h1>
+            <StyledP>아래에서 영화를 선택해 주세요.</StyledP>
             {/* <Select /> */}
             <Select2 />
         </div>
     )
 }
 
-// 2. 좌석 선택 가능하게
+const StyledP = styled.p `
+text-align: center;
+`;
 
-// 1. 선택 불가 좌석 배치하기
+// 2. 좌석 선택 가능하게
