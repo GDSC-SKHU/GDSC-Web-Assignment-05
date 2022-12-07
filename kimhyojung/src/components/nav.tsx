54a5101aae66c7ebcd77b367dba36c0ed198d5d7
khj0426 <706shin1728@naver.com>
2022-12-07 23:39:45 +0900

    style:Nav 스타일 수정

--- kimhyojung/src/components/nav.tsx
+++ kimhyojung/src/components/nav.tsx
@@ -1,27 +1,37 @@
 import styled from 'styled-components';
 import Link from 'next/link';
+
 const StyledhomeNav = styled.nav`
   display: flex;
-  background-color: #444;
+  background-color: #222;
   width: 100%;
   color: white;
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 35px;
+  gap: 15%;
   height: 35%;
-  margin-bottom: 2%;
+  max-height: 150px;
+  margin-bottom: 6%;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
 `;
 
 const StyledhomeLink = styled(Link)`
   color: white;
 `;
 
+const StyledhomeLogo = styled.span`
+  font-size: 1.5rem;
+  color: #fdfdfd;
+`;
+
 export default function Nav() {
   return (
     <>
       <StyledhomeNav>
-        <StyledhomeLink href="/">Home</StyledhomeLink>
+        <StyledhomeLink href="/">홈</StyledhomeLink>
+        <StyledhomeLogo>Carpe diem</StyledhomeLogo>
+        <StyledhomeLink href="/booking">예매</StyledhomeLink>
       </StyledhomeNav>
     </>
   );
