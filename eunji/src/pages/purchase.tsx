4830bb668203da661f3d1dee5a0ab7d0d07a5561
eunji <eunjijjang11@naver.com>
2022-12-12 12:41:30 +0900

    style: 결제창 꾸미기

--- eunji/src/pages/purchase.tsx
+++ eunji/src/pages/purchase.tsx
@@ -5,13 +5,79 @@ export default function Purchase() {
 
     const router = useRouter();
 
-    return(
-        <div>
-            <h1>결제</h1>
-            <h2>선택하신 영화는 {router.query.name}(이)며 인원수는 {router.query.person}명, 총 금액은 {router.query.price}원입니다.</h2>
-        </div>
-    )   
+    const clickPurchase = () => {
+        alert('결제 안 되지롱');
+    }
+
+    return (
+        <Container>
+            <h2>선택하신 영화: {router.query.name}</h2>
+            <Summary>
+                <StyledDiv>
+                    <StyledH3>결제하실 금액</StyledH3>
+                    <StyledP>{router.query.price}원</StyledP>
+                </StyledDiv>
+                <StyledDiv>
+                    <StyledH3>인원수</StyledH3>
+                    <StyledP>{router.query.person}명</StyledP>
+                </StyledDiv>
+            </Summary>
+            <StyledBtn onClick={clickPurchase}>결제하기</StyledBtn>
+        </Container>
+    )
 }
 
-// 무슨 영화, n명, 결제 금액 보여주기
-// 키워드 : router query
+const Container = styled.div`
+margin-left: 1rem;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`;
+
+const Summary = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+const StyledDiv = styled.div`
+width: 10rem;
+height: 5rem;
+margin-right: 1rem;
+margin-bottom: 1rem;
+
+text-align: center;
+border: 1px solid black;
+border-radius: 5px;
+`;
+
+const StyledH3 = styled.h3`
+margin: 0;
+padding: 10px;
+border-bottom: 1px solid black;
+`;
+
+const StyledP = styled.p`
+margin: 0;
+padding: 10px;
+`;
+
+const StyledBtn = styled.button`
+padding: 8px 15px;
+
+background-color: #ad1712;
+color: #fff;
+
+border: 1px solid #ad1712;
+border-radius: 10px;
+
+cursor: pointer;
+
+&:hover {
+    background-color: #fff;
+    color: #ad1712;
+
+    border: 1px solid #ad1712;
+}
+`;
