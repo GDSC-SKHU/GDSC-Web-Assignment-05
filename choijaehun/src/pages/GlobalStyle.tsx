42c0f5024eea5eec4ee33bbf61b30c8d5aeb91a0
zoeyourlife <201814112@office.skhu.ac.kr>
2022-12-14 09:18:54 +0900

    feat: GlobalStyle 적용

--- choijaehun/src/pages/GlobalStyle.tsx
+++ choijaehun/src/pages/GlobalStyle.tsx
@@ -9,13 +9,14 @@ const GlobalStyle = createGlobalStyle`
  }
 
  @font-face {
-    font-family: 'Ansungtangmyun-Bold';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/Ansungtangmyun-Bold.woff2') format('woff2');
+    font-family: 'HSGooltokki';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2210-2@1.0/HSGooltokki.woff2') format('woff2');
     font-weight: normal;
     font-style: normal;
 }
- body{
-   font-family: 'Ansungtangmyun-Bold';
+ body{   
+    font-family: 'HSGooltokki';
+    background-color: #fdfdfd;
  }
 `;
 
