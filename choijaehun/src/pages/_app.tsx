3aa194b16b694280c7cdbcfbce3798cbd32195a1
zoeyourlife <201814112@office.skhu.ac.kr>
2022-12-14 09:19:47 +0900

    style: 전반적인 Nav바만 고정

--- choijaehun/src/pages/_app.tsx
+++ choijaehun/src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import type { AppProps } from "next/app";
 import Link from "next/link";
+import Nav from "../components/Nav";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <nav></nav>
+      <Nav />
       <Component {...pageProps} />
     </>
   );
