c92429ba6bac0ed61c04d06551aef817c9693ec3
sinamong0620 <sinaskim60@gmail.com>
2022-12-14 14:08:56 +0900

    docs:문서 수정

--- kimshinah/src/components/Seating.tsx
+++ kimshinah/src/components/Seating.tsx
@@ -1,24 +1,27 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 interface Props {
   setSeatingCount: (seating: number) => void;
   seatingArray: boolean;
 }
+
 const Seating = ({ setSeatingCount, seatingArray }: Props) => {
-  //여기가 안된다.. ㅠ 좌석이 true에서 false로 넘어가질 않아ㅠ
+  const [newNumber, setnewNumber] = useState(0);
+  //여기가 안된다.. ㅠ 좌석이 true에서 false로 넘어가질 않아ㅠ 올라가질 않음.
   const changeOccupiedStatus = (event: any) => {
-    if (seatingArray === false) {
-      console.log(seatingArray);
-      seatingArray = true;
+    if (newNumber === 0) {
+      console.log(seatingArray + "1");
+      setnewNumber((prev) => prev + 1);
       event.target.style.backgroundColor = "#7ED0FF";
       //number로 지정했는데도 변경안됨.
       setSeatingCount((prev) => prev + 1);
-      console.log(seatingArray);
+      console.log(seatingArray + "2");
     } else {
-      seatingArray = false;
       event.target.style.backgroundColor = "#000000";
-      // setSeatingCount((prev) => prev - 1);
-      console.log(seatingArray);
+      setnewNumber((prev) => prev - 1);
+      setSeatingCount((prev) => prev - 1);
+      console.log(seatingArray + "3");
     }
   };
   return <SeatingCss onClick={changeOccupiedStatus}></SeatingCss>;
