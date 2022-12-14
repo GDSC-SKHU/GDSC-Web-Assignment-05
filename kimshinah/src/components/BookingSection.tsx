88e5b13253c7e5fc9ee1ecc60f8d76f897e6a9e5
sinamong0620 <sinaskim60@gmail.com>
2022-12-14 10:06:11 +0900

    purchase router add

--- kimshinah/src/components/BookingSection.tsx
+++ kimshinah/src/components/BookingSection.tsx
@@ -1,47 +1,96 @@
 import styled from "styled-components";
+import { useState, ChangeEvent, useEffect } from "react";
 import Seating from "../components/Seating";
 import SeatingOccupied from "../components/SeatingOccupied";
+import Router from "next/router";
 
 const BookingSection = () => {
-  const seatingArray = [];
+  //any가 왜있어야 하는걸까...
+  const seatingArray: any = [];
   const seatingOccupied = true;
   const seatingNone = false;
 
+  //
+  const [seatingCount, setSeatingCount] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [movieNumber, setmovieNumber] = useState(3);
+  const [movieName, setmovieName] = useState("엔시티 드림 더 무비");
   //숫자 10 이상으로 랜덤을 넣진 않을 것.
-  const seatingRandomConst = 10;
-  let seatingRandomCount = 0;
+  const seatingOccupiedConst = [23, 24, 26, 32, 33, 36, 37, 38, 45, 46, 51, 53];
 
+  const Movies = [
+    { name: "탄생", price: 1 },
+    { name: "올빼미", price: 2 },
+    { name: "엔시티 드림 더무비", price: 3 },
+    { name: "2022 카타르 월드컵", price: 4 },
+    { name: "압꾸정", price: 5 },
+  ];
+
+  //예외 처리를 꼭 try catch로 해야 하나?
+  const SendQuery = () => {
+    if (seatingCount > 0) {
+      Router.push({
+        pathname: "/purchase",
+        query: {
+          movieNameE: movieName,
+          moviePeople: seatingCount,
+          resultprice: price,
+        },
+      });
+    } else {
+      alert("좌석을 선택해주세요.");
+    }
+  };
+
+  const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setmovieNumber(Number(e.target.value));
+    console.log(Number(e.target.value));
+    setmovieName(Movies[Number(e.target.value) - 1].name);
+    console.log(movieNumber);
+    //그 전이 올빼미였다면 올빼미 번호가 뜨는 식으로  그 이전께 저장 됨ㅋ
+  };
   //일단 배열을 0으로 초기화
-  for (let i = 0; i <= 55; ++i) {
+  for (let i = 0; i < 56; i++) {
     seatingArray.push({ number: i, seatingStatus: seatingNone });
   }
+  //occupied 좌석 설정
+  seatingOccupiedConst.forEach((element) => {
+    seatingArray[element].seatingStatus = seatingOccupied;
+  });
+  useEffect(() => {
+    setPrice(movieNumber * seatingCount);
+  }, [seatingCount]);
 
-  //자리 10개를 occpied로 무작위 선정
-  for (let i = 0; i < seatingRandomConst; ++i) {
-    //랜덤함수를 10번 발동시킨다.
-    const randomCount = Math.floor(Math.random() * 56 + 1);
-    console.log(randomCount);
-    //전체자리를 루프함수로 돌면서 랜덤함수에 해당하는 수의 자리라면 occupied로 바꾼다.
-    for (let i = 0; i <= 55; ++i) {
-      if (randomCount === seatingArray[i].number) {
-        seatingArray[i].seatingStatus = seatingOccupied;
-      }
-    }
-  }
-
-  console.log(seatingArray);
   //map함수에 '=>'이게 인식이 안되었음. 근데 갑자기 { } 중괄호를 넣으니까 갑자기 잘됨 왜지?
   return (
     <BookingMainSection>
+      <select value={movieNumber} onChange={onSelectChange}>
+        <option value={3}>엔시티 드림 더 무비,3원</option>
+        <option value={2}>올빼미,2원</option>
+        <option value={5}>압꾸정,5원</option>
+        <option value={4}>2022 카타르 월드컵,4원</option>
+        <option value={1}>탄생,1원</option>
+      </select>
+
       <BookingGridContainer>
-        {seatingArray.map((element, index) =>
+        {seatingArray.map((element: any, index: any) =>
           element.seatingStatus ? (
             <SeatingOccupied key={index} />
           ) : (
-            <Seating key={index} />
+            <Seating
+              key={index}
+              setSeatingCount={setSeatingCount}
+              seatingArray={seatingArray[index].seatingStatus}
+            />
           )
         )}
       </BookingGridContainer>
+      <div>
+        {seatingCount}개 좌석을 선택했으며 가격은 {movieNumber * seatingCount}원
+        입니다.
+      </div>
+
+      <button onClick={SendQuery}>구매하기</button>
     </BookingMainSection>
   );
 };
--- kimshinah/src/components/Seating.tsx
+++ kimshinah/src/components/Seating.tsx
@@ -1,8 +1,20 @@
 import styled from "styled-components";
+import { useState } from "react";
 
-const Seating = () => {
-  const changeOccupiedStatus = () => {
-    console.log("좌석 선택");
+interface Props {
+  setSeatingCount: (seating: number) => void;
+  seatingArray: boolean;
+}
+const Seating = ({ setSeatingCount, seatingArray }: Props) => {
+  const changeOccupiedStatus = (event: any) => {
+    if (seatingArray == false) {
+      seatingArray = true;
+      event.target.style.backgroundColor = "#7ED0FF";
+      setSeatingCount((prev) => prev + 1);
+    } else {
+      seatingArray = false;
+      event.target.style.backgroundColor = "#000000";
+    }
   };
   return <SeatingCss onClick={changeOccupiedStatus}></SeatingCss>;
 };
